d85b625286f562cf944805ab5096c43d38625311
SirKaelan <kaelanfreakerman6@gmail.com>
2020-09-17 21:32:31 +0300

    Extract method that finds point from radius and degrees

--- src/Model/PolygonShape.cs
+++ src/Model/PolygonShape.cs
@@ -18,18 +18,24 @@ namespace Draw
         {
             var diameter = Math.Min(Rectangle.Width, Rectangle.Height);
             var radius = diameter / 2;
-            var radians = 360 / _points * Math.PI / 180f;
+            var degrees = 360 / _points;
 
             var polygon = new PointF[_points];
             for (int i = 0; i < _points; i++)
             {
-                var pointX = _center.X + radius * (float)Math.Cos(i * radians);
-                var pointY = _center.Y + radius * (float)Math.Sin(i * radians);
-                polygon[i] = new PointF(pointX, pointY);
+                polygon[i] = ConvertDegreesToPoint(radius, i * degrees);
             }
 
             graphics.FillPolygon(fillBrush, polygon);
             graphics.DrawPolygon(drawPen, polygon);
+        }
+
+        private PointF ConvertDegreesToPoint(int radius, double degrees)
+        {
+            var radians = degrees * Math.PI / 180f;
+            var x = _center.X + radius * (float)Math.Cos(radians);
+            var y = _center.Y + radius * (float)Math.Sin(radians);
+            return new PointF(x, y);
         }
     }
 }
